48f54e108deddae4e5c0fd3c5ab503cf0b60a50a
vince <vincedinardo21@gmail.com>
2022-07-12 20:17:43 +0200

    Cards

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,15 @@
 <main>
-   <section class="my-jumbotron">
-        
-   </section> 
+   <section class="my-jumbotron"></section> 
+
+   <!-- Current Series -->
+   <section class="current-series">
+      <div class="card-container">
+         @foreach($comicsArr as $item)
+            <div class="my-card">
+               <img src="{{$item['thumb']}}" alt="">
+               <div>{{$item["title"]}}</div>
+            </div> 
+         @endforeach
+      </div>
+   </section>
 </main>
