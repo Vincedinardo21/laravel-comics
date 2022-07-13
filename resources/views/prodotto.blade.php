e982679f1eccebfcd1234c236bb35df41abc70ac
vince <vincedinardo21@gmail.com>
2022-07-13 21:55:34 +0200

    Fix grafici

--- resources/views/prodotto.blade.php
+++ resources/views/prodotto.blade.php
@@ -14,8 +14,8 @@
                     <img src="{{$comic['thumb']}}" alt="">
                     <h2>{{$comic["title"]}}</h2>
                     <span class="price">
-                        <div>U.S. Price: {{$comic["price"]}}</div>
-                        <div>AVAILABLE</div>
+                        <div><span class="colorlime">U.S. Price: </span>{{$comic["price"]}}</div>
+                        <div class="colorlime">AVAILABLE</div>
                     </span> 
                     <span> <a href="#">Check Availability</a></span>      
                     <p>{{$comic['description']}}</p>
@@ -26,8 +26,26 @@
         <section class="lower">
             <div class="lower-container">
                 <div class="row">
+                    <div class="col-6"><h4>Talent</h4></div>
+                    <div class="col-6"><h4>Specs</h4></div>
+                    <div class="col-6 myflex">
+                        <div>Art by:</div>
+                        <div>Artists</div>
+                    </div>
+                    <div class="col-6 myflex">
+                        <div>Series:</div>
+                        <div class="mycolor">{{$comic["series"]}}</div>
+                    </div>
+                    <div class="col-6"><div>Written by:</div></div>
+                    <div class="col-6 myflex">
+                        <div>U.S. Price:</div>
+                        <div>{{$comic["price"]}}</div>
+                    </div>
                     <div class="col-6"></div>
-                    <div class="col-6"></div>
+                    <div class="col-6 myflex">
+                        <div>On Sale Date:</div>
+                        <div>{{$comic["sale_date"]}}</div>
+                    </div>
                 </div>
             </div>
         </section>
