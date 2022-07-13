63e1cb1a1d5bc9389ad1d22d594d6e148d3cd022
vince <vincedinardo21@gmail.com>
2022-07-13 21:00:18 +0200

    Stile parziale

--- resources/views/prodotto.blade.php
+++ resources/views/prodotto.blade.php
@@ -9,11 +9,27 @@
         </section> 
         
         <section>
-            <img src="{{$comic['thumb']}}" alt="">
-            <h2>{{$comic["title"]}}</h2>
-            <div>U.S. Price: {{$comic["price"]}}</div>
-            <div>AVAILABLE</div>
-            <p>{{$comic['description']}}</p>
+            <div class="info-container">
+                <div class="info-card">
+                    <img src="{{$comic['thumb']}}" alt="">
+                    <h2>{{$comic["title"]}}</h2>
+                    <span class="price">
+                        <div>U.S. Price: {{$comic["price"]}}</div>
+                        <div>AVAILABLE</div>
+                    </span> 
+                    <span> <a href="#">Check Availability</a></span>      
+                    <p>{{$comic['description']}}</p>
+                </div>       
+            </div>     
+        </section>
+
+        <section class="lower">
+            <div class="lower-container">
+                <div class="row">
+                    <div class="col-6"></div>
+                    <div class="col-6"></div>
+                </div>
+            </div>
         </section>
     </main>
 @endsection
